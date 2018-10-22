d42c7e70685cd2add10ecf39b0b5c809e19b4259
hki2345 <hki2345@users.noreply.github.com>
2018-10-22 18:51:08 +0900

    18.10.22 진도 따라집기
    ㅇㅇ

--- Core/KFBX.cpp
+++ Core/KFBX.cpp
@@ -99,81 +99,51 @@ void FBXLoader::Load_Bone(FbxNode* _pNode, KUINT _Depth, KBone* _pParent)
 	{
 		_Depth++;
 	}
+
+	// Child Count - 제귀
+	int CC = _pNode->GetChildCount();
+	for (int i = 0; i < CC; i++)
+	{
+		Load_Bone(_pNode->GetChild(i), _Depth, NBone);
+	}
 }
 
 void FBXLoader::Check_Animation()
 {
+	m_pScene->FillAnimStackNameArray(m_pNewFbx->AniName_Arr);
+
+	for (int i = 0; i < m_pNewFbx->AniName_Arr.GetCount(); i++)
+	{
+		FbxAnimStack* TempStack = m_pScene->FindMember<FbxAnimStack>(m_pNewFbx->AniName_Arr[i]->Buffer());
+
+		// 애니메이션이 없을 수도 있다 -> 건물 같은거
+		if (nullptr == TempStack)
+		{
+			continue;
+		}
+
+		Animation_Info* NewInfo = new Animation_Info();
+		NewInfo->Name = CA2W(TempStack->GetName());
+
+		// 애니메이션의 시작과 끝의 시간을 받아옴
+		FbxTakeInfo* TempInfo = m_pScene->GetTakeInfo(TempStack->GetName());
+		NewInfo->Stime = TempInfo->mLocalTimeSpan.GetStart();
+		NewInfo->Etime = TempInfo->mLocalTimeSpan.GetStop();
+
+		NewInfo->eMode = m_pScene->GetGlobalSettings().GetTimeMode();
+		NewInfo->Length_Time =
+			NewInfo->Etime.GetFrameCount(NewInfo->eMode) - 
+			NewInfo->Stime.GetFrameCount(NewInfo->eMode);
+	}
 
 }
 void FBXLoader::Triangulate(FbxNode* _pNode)
-{
-
-}
-void FBXLoader::Set_MeshData(FbxMesh* _pMesh)
-{
-
-}
-
-
-
-KFBX::KFBX()
-{
-	
-}
-
-
-KFBX::~KFBX()
-{
-}
-
-
-
-void KFBX::Load(const wchar_t* _Path)
-{
-	// 전역으로 선언해 어디서든 볼 수 있게 함ㅇㅇ
-	m_pManager = FbxManager::Create();;
-
-	// 불러올 정보로는
-	// 버텍스(컬러, 노말, 바이노말, 탄젠트, 컬러 등) 인덱스 
-	// 쉐이더
-	// 재질
-	// 1. 텍스쳐
-	// 2. 댑스스텐실- 여기부터는 별도의 정보들 - FBX 에 없다 - 그러니 만들어야함
-	// 3. 블랜드
-	// 4. 레스터라이즈
-
-	// 플렛폼 별 불러오는 세팅이 다르니 그걸 세팅해준다.
-	FbxIOSettings* pIos = FbxIOSettings::Create(m_pManager, IOSROOT);
-	m_pManager->SetIOSettings(pIos);
-
-
-
-	// 내부에서 간헐적으로 신과 임포터를 연다. 어캐보면 박스 2D와 같은 방식
-	FbxScene* pScene = FbxScene::Create(m_pManager, "");
-	FbxImporter* pImp = FbxImporter::Create(m_pManager, "");
-
-	// m_psz -> 자동 string 변환 -> wstring에서 char* 형으로
-	bool Temp = pImp->Initialize(CW2A(_Path, CP_UTF8).m_psz, -1, m_pManager->GetIOSettings());
-	bool Temp2 = pImp->Import(pScene);
-
-
-	Load_Triangle(pScene->GetRootNode());
-
-	pImp->Destroy();
-	pScene->Destroy();
-	pIos->Destroy();
-	m_pManager->Destroy();
-}
-
-
-// FBX라 해도 트리구조로 되어있다.
-void KFBX::Load_Triangle(FbxNode* _pNode)
 {
 	FbxNodeAttribute* pAttr = _pNode->GetNodeAttribute();
 
 	if (nullptr == pAttr)
 	{
-		// BBY;
+		BBY;
 	}
 
 	if (pAttr->GetAttributeType() == FbxNodeAttribute::eMesh)
@@ -184,31 +154,20 @@ void KFBX::Load_Triangle(FbxNode* _pNode)
 
 		// 2번째 bool은 덮어 쓸거냐 말거냐를 묻는다.
 		NewCon.Triangulate(pAttr, true);
-
-		FbxMesh* NewMesh = _pNode->GetMesh();
-		Load_Mesh(NewMesh);
 	}
 
-
-
-	int MatCnt = _pNode->GetMaterialCount();
-
-	if (MatCnt > 0)
+	int CC = _pNode->GetChildCount();
+	for (int i = 0; i < CC; i++)
 	{
-		// 메터리얼 정보 로드
-	}
-
-
-	// 트리구조 -> 자식이 있으면 그걸 제귀돌려서 가져옴
-	for (int i = 0; i < _pNode->GetChildCount(); i++)
-	{
-		Load_Triangle(_pNode->GetChild(i));
+		Triangulate(_pNode->GetChild(i));
 	}
 }
 
 
-void KFBX::Load_Mesh(FbxMesh* _pMesh)
+void FBXLoader::Set_MeshData(FbxMesh* _pMesh)
 {
+	// 메쉬의 정점 개수
+	// 정점의 개수
 	int iVtxCnt = _pMesh->GetControlPointsCount();
 
 	FbxVector4* pVtxPos = _pMesh->GetControlPoints();
